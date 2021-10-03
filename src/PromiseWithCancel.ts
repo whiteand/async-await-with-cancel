f0c1e215d9a03138828bcca53f54212066df5d1e
whiteand <andrewbeletskiy@gmail.com>
2021-10-04 01:13:22 +0300

    try to increase readability of the code

--- src/PromiseWithCancel.ts
+++ src/PromiseWithCancel.ts
@@ -1,89 +1,98 @@
-type functionArgs<U extends unknown[]> = (...args: U) => IterableIterator<unknown>;
+type GeneratorWithArgs<Args extends unknown[]> = (
+  ...args: Args
+) => IterableIterator<unknown>;
 
-export function runWithCancel<T extends unknown[]>(fn: functionArgs<T>, ...args: T): SPromise<unknown>
-{
-    const gen: IterableIterator<unknown> = fn(...args);
-    gen.throw = (err?: unknown) => {
-        return {
-            done: false,
-            value: `error = ${err}`
-        };
+interface GeneratorWithThrowMethod<T> extends Iterator<T, T, unknown> {
+  throw(error?: unknown): IteratorResult<T>;
+}
+
+function runGenerator<Args extends unknown[]>(
+  fn: GeneratorWithArgs<Args>,
+  args: Args
+): GeneratorWithThrowMethod<unknown> {
+  const gen: IterableIterator<unknown> = fn(...args);
+  gen.throw = (err?: unknown) => {
+    return {
+      done: false,
+      value: `error = ${err}`,
+    };
+  };
+
+  return gen as GeneratorWithThrowMethod<unknown>;
+}
+
+export function runWithCancel<Args extends unknown[]>(
+  fn: GeneratorWithArgs<Args>,
+  ...args: Args
+): CancellablePromise<unknown> {
+  const gen = runGenerator(fn, args);
+
+  let cancelled: boolean = false;
+  let cancel: (() => void) | null = null;
+
+  const promise = new CancellablePromise<unknown>((resolve, reject) => {
+    // Set cancel function to return it from our fn
+    cancel = () => {
+      cancelled = true;
+      reject({ reason: "cancelled" });
     };
 
-    let cancelled: boolean = false;
-    let cancel: (() => void) | null = null;
+    // The first run of the onFulfilled function.
+    onFulfilled();
 
-    const promise = new SPromise<unknown>((resolve, reject) =>
-    {
-        // Set cancel function to return it from our fn
-        cancel = () => {
-            cancelled = true;
-            reject({reason: 'cancelled'});
-        };
+    function onFulfilled(res?: unknown): unknown {
+      if (cancelled) return;
 
-
-        // The first run of the onFulfilled function.
-        onFulfilled();
-
-        function onFulfilled<U>(res?: U)
-        {
-            if (!cancelled) {
-                let result: IteratorResult<unknown>;
-
-                try {
-                    result = gen.next(res);
-                } catch (e) {
-                    return reject(e);
-                }
-
-                next(result);
-
-                return null;
-            }
-        }
-
-        function onRejected(err: any)
-        {
-            let result: IteratorResult<unknown>;
-
-            try {
-                // assert(gen.throw);
-                result = (gen.throw as ((e?: any) => IteratorResult<T>))(err);
-            } catch (e) {
-                return reject(e);
-            }
-
-            next(result);
-        }
-
-
-        // Here we resolve promise and recursively run onFulfilled/onRejected again
-        function next({done, value}: { done: boolean, value: unknown })
-        {
-            if (done)
-                return resolve(value);
-
-            return Promise.resolve(value).then(<U>(res: U) => {
-                onFulfilled(res);
-            }, <U>(res: U) => {
-                onRejected(res);
-            });
-        }
-    });
-
-    promise.cancelFunc = cancel;
-
-    return promise;
-}
-
-export class SPromise<T> extends Promise<T>
-{
-    public cancelFunc: (() => void) | null = null;
-
-    public cancel()
-    {
-        if (!this.cancelFunc) throw new Error('SPromise can only be used with runWithCancel');
-        this.cancelFunc();
+      try {
+        const result = gen.next(res);
+        next(result);
+        return null;
+      } catch (e) {
+        return reject(e);
+      }
     }
+
+    function onRejected(err: any): void {
+      let result: IteratorResult<unknown>;
+
+      try {
+        result = gen.throw(err);
+      } catch (e) {
+        return reject(e);
+      }
+
+      next(result);
+    }
+
+    // Here we resolve promise and recursively run onFulfilled/onRejected again
+    function next<T>({ done, value }: { done?: boolean; value: T }): void {
+      if (done) {
+        resolve(value);
+        return;
+      }
+
+      Promise.resolve(value).then(
+        (res: T) => {
+          onFulfilled(res);
+        },
+        (res: unknown) => {
+          onRejected(res);
+        }
+      );
+    }
+  });
+
+  promise.cancelFunc = cancel;
+
+  return promise;
 }
 
+export class CancellablePromise<T> extends Promise<T> {
+  public cancelFunc: (() => void) | null = null;
+
+  public cancel() {
+    if (!this.cancelFunc)
+      throw new Error("CancellablePromise can only be used with runWithCancel");
+    this.cancelFunc();
+  }
+}
